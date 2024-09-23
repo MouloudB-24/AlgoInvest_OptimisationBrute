73323e017f5364f5d650d4542f36503edae12099
Mouloud BELLIL <mouloud.bellil@outlook.fr>
2024-09-23 06:12:31 +0200

    style(bruteforce): Rename functions

--- bruteforce.py
+++ bruteforce.py
@@ -30,33 +30,33 @@ Stratégie :
 
 
 # Utilisation de l'algorithme Powerset
-def get_combinations(actions):
+def get_combinations(data):
     # Initialise la liste des combinaisons avec un combinaison vide
-    combinations = [[]]
+    all_combinations = [[]]
 
-    for action in actions:
+    for element in data:
         # Créer une nouvelle liste pour stocker les combinaisons générées
         new_combinations = []
 
-        for combination in combinations:
+        for combination in all_combinations:
             # On ajoute uniqument la combinaisons
             new_combinations.append(combination)
 
             # On ajoute la combinaison + l'élément actuel
-            new_combinations.append(combination + [action])
+            new_combinations.append(combination + [element])
 
-        # Mettre à jour les combinaisons pour inclure les nouvelles
-        combinations = new_combinations
+        # Mettre à jour les combinaisons pour inclure les nouvelles combinaisons générées
+        all_combinations = new_combinations
 
-    return combinations
+    return all_combinations
 
 
-def get_target_combinations(target, combinations):
-    target_combinations = []
-    for combination in combinations:
+def get_target_combinations(target, all_combinations):
+    combinations = []
+    for combination in all_combinations:
         if sum(combination) == target:
             combinations.append(combination)
-    return target_combinations
+    return combinations
 
 
 def calculate_profit(combinations):
@@ -67,6 +67,5 @@ if __name__ == "__main__":
     actions = [20, 30, 50, 70, 60, 80, 22, 26, 48, 34, 42, 38, 14, 18, 8, 4, 10, 24, 114]
     profit = [0.05, 0.1, 0.15, 0.2, 0.17, 0.25, 0.07, 0.11, 0.13, 0.27, 0.17, 0.09, 0.23, 0.01, 0.03, 0.08, 0.12, 0.14, 0.21]
     target = 500
-    print(len(profit))
-    combinations = get_combinations(target, actions)
-    print(f"Nomber of possible combinations is: {len(combinations)}\n{combinations}")
+    all_combinations = get_combinations(actions)
+    print(f"Nomber of possible combinations is: {len(all_combinations)}")
