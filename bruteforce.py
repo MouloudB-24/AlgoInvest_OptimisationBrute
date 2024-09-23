4f0d40b528abaf7ba26b4cb487191716dacc201f
Mouloud BELLIL <mouloud.bellil@outlook.fr>
2024-09-23 05:56:25 +0200

    feat(bruteforce): Add get_target_combinations function

--- bruteforce.py
+++ bruteforce.py
@@ -21,18 +21,16 @@ Objectif :
 Stratégie :
 --> Trouver toutes les combinaisons de liste possible dans la liste d'actions.
 
---> Parcouris toutes les combinaisons trouvée, garder celles dont la valeur == 500€
+--> Parcouris toutes les combinaisons trouvée, garder uniquement celles dont la valeur == 500€
 
---> Claculer pour chaque combinaison les bénéfices en mutipliant par le %
+--> Claculer pour chaque combinaison les bénéfices en mutipliant par le pourcentage %
 
 --> Comparer les bénéfices en choisissant la meilleure combinaisons d'actions
 """
 
 
 # Utilisation de l'algorithme Powerset
-def get_combinations(target, actions):
-    result = []
-
+def get_combinations(actions):
     # Initialise la liste des combinaisons avec un combinaison vide
     combinations = [[]]
 
@@ -50,11 +48,15 @@ def get_combinations(target, actions):
         # Mettre à jour les combinaisons pour inclure les nouvelles
         combinations = new_combinations
 
+    return combinations
+
+
+def get_target_combinations(target, combinations):
+    target_combinations = []
     for combination in combinations:
         if sum(combination) == target:
-            result.append(combination)
-
-    return result
+            combinations.append(combination)
+    return target_combinations
 
 
 def calculate_profit(combinations):
@@ -63,6 +65,8 @@ def calculate_profit(combinations):
 
 if __name__ == "__main__":
     actions = [20, 30, 50, 70, 60, 80, 22, 26, 48, 34, 42, 38, 14, 18, 8, 4, 10, 24, 114]
+    profit = [0.05, 0.1, 0.15, 0.2, 0.17, 0.25, 0.07, 0.11, 0.13, 0.27, 0.17, 0.09, 0.23, 0.01, 0.03, 0.08, 0.12, 0.14, 0.21]
     target = 500
+    print(len(profit))
     combinations = get_combinations(target, actions)
     print(f"Nomber of possible combinations is: {len(combinations)}\n{combinations}")
