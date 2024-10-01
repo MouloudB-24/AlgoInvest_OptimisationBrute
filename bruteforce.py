45daba410ded7df4be50ac2adf73410d50e5ebfc
Mouloud BELLIL <mouloud.bellil@outlook.fr>
2024-10-01 17:04:06 +0200

    style: Renommer quelques fonctions et variables

--- bruteforce.py
+++ bruteforce.py
@@ -19,7 +19,7 @@ Objectif :
 --> Trouver toutes les combinaisons possibles d'actions dont la valeur == 500€
 
 Stratégie :
---> Trouver toutes les combinaisons de liste possible dans la liste d'actions.
+--> Trouver toutes les combinaisons possible dans la liste d'actions.
 
 --> Parcouris toutes les combinaisons trouvée, garder uniquement celles dont la valeur <= 500€
 
@@ -30,12 +30,9 @@ Stratégie :
 import csv
 import time
 from pathlib import Path
-from tqdm import tqdm
-from memory_profiler import profile
 
 
 # Function to retrieve data from a CSV file
-#@profile
 def read_csv(file_name):
     """
     Retrieve data from a CSV file.
@@ -43,33 +40,32 @@ def read_csv(file_name):
     :param file_name: Name file.
     :return: Data in list form.
     """
-    data = []
+    shares = []
     folder = Path("data") / file_name
     with open(folder, "r", newline="", encoding="utf-8") as file:
         raw_data = csv.reader(file)
 
-        for row in tqdm(raw_data, desc="Reading CSV data"):
+        for row in raw_data:
             percentage = row[2].strip("%")
-            data.append([row[0], row[1], percentage])
+            shares.append([row[0], row[1], percentage])
 
     # Delete headers
-    data.pop(0)
-    return data
+    shares.pop(0)
+    return shares
 
 
 # Function to calculate all possible combinations
-#@profile
-def find_all_combinations(data):
+def brute_force_algorithm(shares):
     """
     Calculate all possible sub-lists combinations in the data list.
 
-    :param data: The list whose possible sub-lists are to be calculated.
+    :param shares: The list whose possible sub-lists are to be calculated.
     :return: The list containing all sub-lists.
     """
     # Initialize the list of combinations
     all_combinations = [[]]
 
-    for element in tqdm(data, desc="Generating all combinations"):
+    for element in shares:
         new_combinations = []
 
         for combination in all_combinations:
@@ -82,23 +78,22 @@ def find_all_combinations(data):
 
 
 # Function to calculate all target combinations
-#@profile
-def get_target_combinations(all_combinations, target):
+def get_target_combinations(all_combinations, budget):
     """
     Obtain the target combinations from the list of all possible combinations.
 
     :param all_combinations: The list of th all possible combinations.
-    :param target: The target of the combinations chosen, which represents a sum.
+    :param budget: The target of the combinations chosen, which represents a sum.
     :return: The list containing the target combinations
     """
     target_combinations = []
-    for combination in tqdm(all_combinations, desc="Obtain target combinations"):
+    for combination in all_combinations:
         current_target = 0
-        for action in combination:
-            current_target += int(action[1])
-            if current_target > target:
+        for share in combination:
+            current_target += int(share[1])
+            if current_target > budget:
                 break
-        if current_target <= target:
+        if current_target <= budget:
             target_combinations.append(combination)
     return target_combinations
 
@@ -112,10 +107,10 @@ def calculate_profit_combination(target_combinations):
     :return: The list of combinations with profits.
     """
     profit_combinations = []
-    for combination in tqdm(target_combinations, desc="Calculating profits"):
+    for combination in target_combinations:
         new_combination = []
-        for action in combination:
-            new_combination.append((action[0], int(action[1]), int(action[1]) * int(action[2]) / 100))
+        for share in combination:
+            new_combination.append((share[0], int(share[1]), int(share[1]) * int(share[2]) / 100))
         profit_combinations.append(new_combination)
     return profit_combinations
 
@@ -128,35 +123,30 @@ def get_best_combination(combinations):
     :param combinations: The list of the combinations.
     :return: The best combinations if action list and benefits
     """
-    best_cost = 0
-    best_profit = 0
+    cost = 0
+    profit = 0
     best_combination = []
-    for combination in tqdm(combinations, desc="Finding the best combination"):
-        profit = 0
-        cost = 0
-        for action in combination:
-            cost += action[1]
-            profit += action[2]
-        if profit > best_profit:
-            best_profit = profit
-            best_cost = cost
+    for combination in combinations:
+        current_cost = 0
+        current_profit = 0
+        for share in combination:
+            current_cost += share[1]
+            current_profit += share[2]
+        if current_profit > profit:
+            profit = current_profit
+            cost = current_cost
             best_combination = combination
-    return f"The best cost: {best_cost}€\nThe best profit: {best_profit}€\nThe best combination : {best_combination}"
+    return cost, profit, best_combination
 
 
 if __name__ == "__main__":
-    # Début de l'éxecution de l'algorithme
     start_time = time.time()
     data = read_csv("actions.csv")
-    all_combinations = find_all_combinations(data)
-    #print(f"Nomber of possible combinations is: {len(all_combinations)}\n{all_combinations[77]}")
+    all_combinations = brute_force_algorithm(data)
     combinations = get_target_combinations(all_combinations, 500)
-    #print(f"Combinaisons ciblée : {len(combinations)}")
-    #print(f"Les données : {data}")
     profit_per_combinations = calculate_profit_combination(combinations)
-    #print(f"Les combinaisons avec le bénéfice: {profit_per_combinations}")
-    best_combination = get_best_combination(profit_per_combinations)
-    # Début de l'éxecution de l'algorithme
+    cost, profit, best_combination = get_best_combination(profit_per_combinations)
     end_time = time.time()
     print(f"Execution time: {end_time - start_time} seconds.")
-    print(best_combination)
+    print(f"Coût : {cost}€\nBénéfice : {profit}€\nActions : {best_combination}")
+
